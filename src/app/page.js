f0b229a990387b2ce48ba801e00dba883c9dcb42
ded1l <tahaabbas987@gmail.com>
2023-11-30 01:10:29 +0300

    1s v0.2

--- src/app/page.js
+++ src/app/page.js
@@ -9,27 +9,30 @@ import { useState ,useEffect } from 'react'
 import Footer from '@/components/footer/Footer'
 
 export default function Home() {
-
+const [loading,setLoading]=useState(false)
 const [info,setInfo]=useState([])
+//
 
-  const getData=async()=>{
-    try {
-      let res =await fetch(`https://api.slingacademy.com/v1/sample-data/blog-posts?limit=9&offset=0`)
-      let data=await res.json()
-     
-      setInfo(data.blogs)
-      console.log(info);
-      
-    } catch (error) {
-      console.log(error)
-      
-    }
+const getData = async () => {
+  try {
+    setLoading(true);
+    let res = await fetch(`https://api.slingacademy.com/v1/sample-data/blog-posts?limit=9&offset=0`);
+    let data = await res.json();
+    setInfo(data.blogs);
+    setLoading(false); // also set loading to false if an error occurs
+// set loading to false after data is fetched
+  } catch (error) {
+    console.log(error);
   }
+}
 useEffect(() => {
   getData()
 }
 , [info.blogs]) 
 
+// if (loading) {
+//   return <div>Loading...</div>; // render a loading message while data is being fetched
+// }
 
   return (
     <main className={styles.main}>
@@ -44,10 +47,16 @@ useEffect(() => {
 <Container>
   
   <div className={styles.grid}>
-    
+ 
+
   {info.map((e, index) => (
+    
                     <Card key={index} title={e.title} subTitle={e.category} img={e.photo_url} id={e.id} date={e.created_at} />
                 ))}
+    </div> <div className={styles.load}>
+     {loading ? <div className={styles.loader}></div>: ""
+  }
+
     </div>
     </Container>
   <Footer/>
